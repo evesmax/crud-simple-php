6f36eb0711c19617f432f5f15772d3d18a7aa9b5
unknown <mhernandez060402@gmail.com>
2024-02-20 08:38:07 -0600

    Actualizamos parametros de conexion C

--- config.php
+++ config.php
@@ -3,12 +3,12 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '0178Alexismax');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'demo');
-define('DB_PORT','3306');
+define('DB_PORT','3308');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
  
 // Check connection
 if($link === false){
