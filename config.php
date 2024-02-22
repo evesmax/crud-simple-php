c277283ba89b21f4de5c66041c655b544b6e8428
AbrahamM010 <abrahammendoza.i.am@gmail.com>
2024-02-22 08:49:06 -0600

    Cambios hechos

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
+//hola
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '0178Alexismax');
