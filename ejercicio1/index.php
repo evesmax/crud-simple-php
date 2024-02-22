7dd5dd825a0b016c086aac49055aa59b7fd84b14
Parker050 <alanlisti55@gmail.com>
2024-02-22 08:02:34 -0600

    Ejercicios de practica

---
+++ ejercicio1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>Consultar</div>
+    <?php
+    require_once "config1.php";
+    echo "<a href='base1.php'>BASE DE DATOS 1</a>";
+    echo "<br>";echo "<br>";
+    echo "<a href='base2.php'>BASE DE DATOS 2</a>";
+    echo "<br>";echo "<br>";
+    echo "<a href='base3.php'>BASE DE DATOS 3</a>";
+    ?>
+</body>
+</html>
