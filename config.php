deb6d093667deabda123946205f0fc7620fd4fb5
unknown <mhernandez060402@gmail.com>
2024-02-22 08:05:07 -0600

    Mensaje C

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'PinguinO79');
 define('DB_NAME', 'demo');
 define('DB_PORT','3308');
  
