09ba1b013932518cd4ddde3f8eb0d7af4a1a1a04
Jose <jquinonesgaribay@gmail.com>
2024-02-22 06:30:11 -0600

    Cambios Aplicados

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+server with default setting (user 'root' with no password) Hola!! */
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'Root');
