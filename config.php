201f16bec7fd4a0cd3201bcf830fc88edcc3534c
AndresArevalo1229 <andres.arevalonavarro@gmail.com>
2024-02-27 08:09:09 -0600

    cambios aplicados en el config

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ server with default setting (user 'root' with no password) */
 $db=$_SESSION['db'];
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '0178Alexismax');
+define('DB_PASSWORD', '');
 define('DB_NAME', $db);
-define('DB_PORT','3306');
-
+define('DB_PORT','3308');
+//adfdfef
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_NAME, DB_PORT);
  
